cfa7008e90868fa4d85bed6368544c98803abca0
Hayley Dixon <hdixon37@gmail.com>
2021-05-18 14:16:57 -0600

    Complete tweet class

--- tweet.js
+++ tweet.js
@@ -1,5 +1,22 @@
 class Tweet {
-
+  constructor(sender, content, retweets, replies, replyCount) {
+    this.sender = sender;
+    this.content = content;
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+    }
+  like() {
+    this.likes = this.likes + 1;
+    }
+  retweet() {
+    this.retweets = this.retweets + 1;
+    }
+  reply(reply){
+    this.replies.push(reply);
+    this.replyCount = this.replies.length;
+  }
 }
 
 module.exports = Tweet;
